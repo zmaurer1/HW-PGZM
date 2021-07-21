820c74d0635d92edf1193cd0cafb1ad0ce62856e
Zach Maurer <zmaurer88@yahoo.com>
2021-07-20 22:08:59 -0700

    Update

--- script.js
+++ script.js
@@ -1,48 +1,58 @@
-// Assignment Code
-var upperCase = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
-console.log(upperCase);
-var lowerCase = `abcdefghijklmnopqrstuvwxyz`;
-console.log(lowerCase);
-var specChar = `!@#$%^&*?`;
-console.log(specChar);
-var numbers = `0123456789`;
-console.log(numbers);
-
 var generateBtn = document.querySelector("#generate");
-function generatePassword() {
-  console.log("final");
+var length;
+var lowercase;
+var uppercase;
+var special;
+var numeric;
+var characters = "";
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
+
+
+function getLength() {
+  length = prompt("Password length");
+  if(length < 8) {
+    getLength()
+  }
+  return length;
 }
-// Write password to the #password input
+
+function generatePassword() {
+  getLength()
+  if(prompt("Use uppercase characters? (y/n)") === "y") {
+    characters += uppercaseLetters;
+    console.log(characters)
+  }
+  if(prompt("Use lowercase characters? (y/n)") === "y") {
+    characters += lowercaseLetters;
+    console.log(characters)
+  }
+  if(prompt("Use numbers? (y/n)") === "y") {
+    characters += numbers;
+    console.log(characters)
+  }
+  if(prompt("Use special characters? (y/n)") === "y") {
+    characters += symbols;
+    console.log(characters)
+  }
+  var password = "";
+  for(x = 0; x < length; x++) {
+    console.log(x)
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+    console.log(password)
+  }
+  return password
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var promptOneAnswer = prompt("Would you like to use number characters? Enter Y or N");
-  var promptTwoAnswer = prompt("Would you like to use special characters? Enter Y or N");
-  passwordText.value = prompt("Would you like to use number characters? Enter Y or N");
-  var passwordLength = prompt("How many characters would you like your password to be?");
-  
-  console.log(promptOneAnswer);
-  console.log(promptTwoAnswer);
-  console.log(passwordLength);
-}
 
-for (var i = 0; i<passwordLength; i++) {
-
-var userChoice = Math.floor(Math.random[])*4);
-
-if (userChoice ==0) {
-    
-}
-    
-    
+  passwordText.value = password;
 
 }
-
-
-
-if (promptOneAnswer===N) {
-
-
-}
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
